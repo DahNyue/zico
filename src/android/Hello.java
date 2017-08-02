0a242ef195197335521ae78f2a4ca9e54c6cfe72
DahNyue <dahnyue@gmail.com>
2017-08-03 01:52:33 +0900

    Add files via upload

---
+++ src/android/Hello.java
@@ -0,0 +1,37 @@
+package com.example.plugin;
+
+import org.apache.cordova.*;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+public class Hello extends CordovaPlugin {
+
+    @Override
+    public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
+
+        if (action.equals("greet")) {
+
+            String name = data.getString(0);
+            String message = "Hello, " + name;
+            callbackContext.success(message);
+
+            return true;
+
+        } else if (action.equals("echo")) {
+			String message = args.getString(0);
+			this.echo(message, callbackContext);
+			return true;
+		} else {
+            
+            return false;
+
+        }
+    }
+	private void echo(String message, CallbackContext callbackContext) {
+		if (message != null && message.length() > 0) {
+			callbackContext.success(message);
+		} else {
+			callbackContext.error("Expected one non-empty string argument.");
+		}
+	}
+}
